881c088ff439c14c44ef3a68cb06f3753080fb7a
Mc <mc@MacBook-Air-Mac-2.local>
2023-05-05 20:33:48 +0600

    first commit

---
+++ lesson6/bin/lesson6.dart
@@ -0,0 +1,7 @@
+import 'package:lesson6/lesson6.dart' as lesson6;
+
+void main() {
+
+}
+ 
+ 
---
+++ lesson6/test/lesson6_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson6/lesson6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
